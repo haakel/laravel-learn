8a9794fc52a158ff3eea37dfbd45821d8138ccd0
haakel <haakel213@gmail.com>
2025-07-31 16:03:33 +0330

    i really dont know just do it - 1:43:00  https://www.youtube.com/watch?v=MG1kt_wiIz0

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,39 +1,49 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-
-
                     <ul
                         class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
+
                         <li class="me-2">
-                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
-                                aria-current="page">Tab 1</a>
+                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active">
+                                all</a>
                         </li>
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
-                                2</a>
-                        </li>
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
-                                3</a>
-                        </li>
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
-                                4</a>
-                        </li>
-                        <li>
-                            <a class="inline-block px-4 py-3 text-gray-400 cursor-not-allowed dark:text-gray-500">Tab
-                                5</a>
-                        </li>
+                        @foreach ($categories as $category)
+                            <li class="me-2">
+                                <a href="#"
+                                    class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
+                                    {{ $category->name }}
+                                </a>
+                            </li>
+                        @endforeach
+
                     </ul>
+
 
                 </div>
             </div>
         </div>
+
+    </div>
+
+
+    <div class="py-1">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-7">
+            <div class="mt-8 text-gray-900">
+                <div class="p-6 ">
+                    @forelse ($posts as $post)
+                        <x-post-item :post="$post"></x-post-item>
+                    @empty <div class="text-center text-gray-500 py-16">
+                            No posts available.
+                        </div>
+                    @endforelse
+                </div>
+                {{ $posts->onEachSide(1)->links() }}
+
+            </div>
+        </div>
+
     </div>
 </x-app-layout>
